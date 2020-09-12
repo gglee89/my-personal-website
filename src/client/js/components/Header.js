d39825cb3593b82619e0dce7f6d3332fb62937af
Gustavo Lee <gglee89@gmail.com>
2020-09-12 17:14:58 -0400

    feat: Include 'onClick' to mobile-nav & mobile-nav-toggle

--- src/client/js/components/Header.js
+++ src/client/js/components/Header.js
@@ -1,43 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
+const navContent = () => {
+  return (
+    <ul>
+      <li>
+        <a href="#about">About</a>
+      </li>
+      <li>
+        <a href="#youtube">YouTube</a>
+      </li>
+      <li>
+        <a href="#projects">Projects</a>
+      </li>
+      <li>
+        <a href="#interests">Interests</a>
+      </li>
+      <li>
+        <a href="#mission">Mission</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  );
+};
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <React.Fragment>
+      <div className={`mobile-nav ${isOpen ? 'is-open' : ''}`}>
+        {navContent()}
+      </div>
       <header>
         <div className="header-position">
           <h1 className="logo hide-text">Giwoo Gustavo Lee</h1>
-          <nav className="site-nav">
-            <ul>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#youtube">YouTube</a>
-              </li>
-              <li>
-                <a href="#projects">Projects</a>
-              </li>
-              <li>
-                <a href="#interests">Interests</a>
-              </li>
-              <li>
-                <a href="#mission">Mission</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </nav>
+          <nav className="site-nav">{navContent()}</nav>
           <a href="mailto:gglee89@gmail.com" className="email-link">
             +email me
           </a>
-          <div className="mobile-nav-toggle">
+          <div
+            className={`mobile-nav-toggle ${isOpen ? 'is-open' : ''}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <span></span>
           </div>
         </div>
       </header>
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
 
 export default Header;
